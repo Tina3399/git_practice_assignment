8a632941aa23f71499131cfcfc5db8bfa80c2dce
Tina Rawat <tinarawat03@gmail.com>
2022-10-21 18:39:35 +0530

    problem2-palindrome

---
+++ palindrome_problem.js
@@ -0,0 +1,13 @@
+let str = "naman";
+let flag = true;
+for (let i = 0; i < str.length; i++) {
+  if (str[i] !== str[str.length - 1 - i]) {
+    flag = false;
+    break;
+  }
+}
+if (flag) {
+  console.log("not palindrome");
+} else {
+  console.log("it is a palindrome");
+}
